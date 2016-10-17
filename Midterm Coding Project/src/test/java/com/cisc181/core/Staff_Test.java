c9060b08af5b201f3b7a1387d1755fff95e0a2e3
Austin <Austin@AustinsPC>
2016-10-17 00:36:51 -0400

    Midterm

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -10,13 +10,25 @@ import org.junit.Test;
 
 public class Staff_Test {
 
-	@BeforeClass
-	public static void setup() {
-	}
-	
-	@Test
-	public void test() {
-		assertEquals(1,1);
-	}	
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
 
+	@Test
+	
+	public void test() throws PersonException {
+		Staff Mike = new Staff("Mike", "Bobby", "Joey", new GregorianCalendar(1994, 8, 17).getTime(), "302 David Hollowell", "55-555-5555", "Mike@udel", "never", 1, 1000, null, null);
+		Staff Joe = new Staff(null, null, null, new GregorianCalendar(1995, 8, 17).getTime(), null, "555-555-5555", null, null, 2, 2000, null, null);
+		Staff Bob = new Staff(null, null, null, new GregorianCalendar(1996, 8, 17).getTime(), null, "555-555-5555", null, null, 3, 3000, null, null);
+		Staff Sally = new Staff(null, null, null, new GregorianCalendar(1800, 8, 17).getTime(), null, "555-555-5555", null, null, 4, 4000, null, null);
+		Staff Ally = new Staff(null, null, null, new GregorianCalendar(1997, 8, 17).getTime(), null, "55-555-5555", null, null, 5, 5000, null, null);
+		
+    ArrayList<Staff> people = new ArrayList<Staff>();
+    people.add(Mike);
+    people.add(Joe);
+    people.add(Bob);
+    people.add(Sally);
+    people.add(Ally);
+    
+    
+    assertEquals((people.get(0).getSalary() + people.get(1).getSalary() + people.get(2).getSalary() + people.get(3).getSalary() + people.get(4).getSalary())/5,3000)
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -10,12 +10,32 @@ import org.junit.Test;
 
 public class Student_Test {
 
-	@BeforeClass
-	public static void setup() {
-	}
-
 	@Test
-	public void test() {
-		assertEquals(1, 1);
-	}
+	public void test() throws PersonException {
+		Student Joe = new Student("bob","ross" , "bob", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.COMPSI, null, "555-555-5555", null);
+		Student Rick = new Student("joe", "nick", "forton", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.COMPSI, null, "5555555555", null);
+		Student Mike = new Student("jim", "bob", "Halpert", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.PHYSICS, null, "5555555555", null);
+		Student Josh = new Student("Drake","and" , "Josh", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.PHYSICS, null, "555-555-5555", null);
+		Student James = new Student("Austin","Joseph" , "Barron", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.PHYSICS, null, "555-555-5555", null);
+		Student Jake = new Student("James","Steven" , "Barron", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.PHYSICS, null, "555-555-5555", null);
+		Student Joanna = new Student("Courtney","stevens" , "brown", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.CHEM, null, "555-555-5555", null);
+		Student John = new Student("Casey","Elizabeth" , "Grzinic", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.CHEM, null, "555-555-5555", null);
+		Student Jacob = new Student("Tim","James" , "allen", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.CHEM, null, "555-555-5555", null);
+		Student Rob = new Student("frank","James" , "lampard", new GregorianCalendar(1996, 8, 17).getTime(), eMajor.CHEM, null, "555-555-5555", null);
+		
+	    ArrayList<Student> people = new ArrayList<Student>();
+	    people.add(Joe);
+	    people.add(Rick);
+		people.add(Mike);
+	    people.add(Josh);
+	    people.add(James);
+	    people.add(Jake);
+	    people.add(Joanna);
+	    people.add(John);
+	    people.add(Jacob);
+	    people.add(Rob);
+	    
+	    people.get(0).setMajor(eMajor.CHEM);
+	    assertEquals(people.get(0).getMajor(),eMajor.CHEM);
+	
 }
